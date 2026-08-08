8da6fd7a0301c971e7a5c38ea5221d54db15fcfa
Jonas <xormios@hotmail.com>
2026-08-08 15:40:38 +0200

    Security setup fixes

--- workouts-common/src/main/java/com/xormios/workouts/common/repository/UserRepository.java
+++ workouts-common/src/main/java/com/xormios/workouts/common/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.xormios.workouts.common.repository;
 
 import com.xormios.workouts.common.entity.ApplicationUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,7 +11,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<ApplicationUser,Long> {
 
-    Optional<ApplicationUser> findByUsername(String username);
+    @Query("SELECT u FROM ApplicationUser u LEFT JOIN FETCH u.roles WHERE u.username = :username")
+    Optional<ApplicationUser> findByUsername(@Param("username") String username);
 
     boolean existsByUsername(String username);
 }
--- workouts-v1-api/src/main/java/com/xormios/workouts/controller/WorkoutController.java
+++ workouts-v1-api/src/main/java/com/xormios/workouts/controller/WorkoutController.java
@@ -1,10 +1,10 @@
 package com.xormios.workouts.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/workout")
 public class WorkoutController {
 
